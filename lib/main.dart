4e935a529e1235c374ff1c871e3ac1eb7be1af6f
Tom ding <dingwensheng@changingpay.com>
2018-07-04 17:58:56 +0800

    rename variable add account page

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,29 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new ExampleWidget(),
+      home: new PasswordListWidget(),
     );
   }
 }
 
 
 /// Opens an [AlertDialog] showing what the user typed.
-class ExampleWidget extends StatefulWidget {
-  ExampleWidget({Key key}) : super(key: key);
+class PasswordListWidget extends StatefulWidget {
+  PasswordListWidget({Key key}) : super(key: key);
 
   @override
-  _ExampleWidgetState createState() => new _ExampleWidgetState();
+  _PasswordListWidgetState createState() => new _PasswordListWidgetState();
 }
 
-/// State for [ExampleWidget] widgets.
-class _ExampleWidgetState extends State<ExampleWidget> {
+/// State for [PasswordListWidget] widgets.
+class _PasswordListWidgetState extends State<PasswordListWidget> {
   final TextEditingController _controller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // There is a input Field for search the passwords
         title: new TextField(
           controller: _controller,
           decoration: new InputDecoration(
@@ -55,15 +54,7 @@ class _ExampleWidgetState extends State<ExampleWidget> {
               icon: new Icon(Icons.search)
           ),
           new IconButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => new AlertDialog(
-                    title: new Text('What you typed'),
-                    content: new Text(_controller.text),
-                  ),
-                );
-              },
+              onPressed: _passwordAdd,
               icon: new Icon(Icons.person_add)
           ),
         ],
@@ -76,5 +67,15 @@ class _ExampleWidgetState extends State<ExampleWidget> {
       },
       ),
     );
+  }
+  void _passwordAdd(){
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context){
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Add New Acount"),
+        ),
+        body: new Text("this is test"),
+      );
+    }));
   }
 }
