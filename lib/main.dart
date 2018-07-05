984dd5e2df7dcb64d67c370e693a328602dd4875
Tom ding <dingwensheng@changingpay.com>
2018-07-05 18:05:10 +0800

    update add account page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 /// Opens an [AlertDialog] showing what the user typed.
 class PasswordListWidget extends StatefulWidget {
   PasswordListWidget({Key key}) : super(key: key);
@@ -46,35 +45,72 @@ class _PasswordListWidgetState extends State<PasswordListWidget> {
                 showDialog(
                   context: context,
                   builder: (context) => new AlertDialog(
-                    title: new Text('What you typed'),
-                    content: new Text(_controller.text),
-                  ),
+                        title: new Text('What you typed'),
+                        content: new Text(_controller.text),
+                      ),
                 );
               },
-              icon: new Icon(Icons.search)
-          ),
+              icon: new Icon(Icons.search)),
           new IconButton(
-              onPressed: _passwordAdd,
-              icon: new Icon(Icons.person_add)
-          ),
+              onPressed: _accountAdd, icon: new Icon(Icons.person_add)),
         ],
       ),
-      body: new ListView.builder(itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        return new ListTile(
-          title: new Text("list title ${i.toString()}"),
-        );
-      },
+      body: new ListView.builder(
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+          return new ListTile(
+            title: new Text("list title ${i.toString()}"),
+          );
+        },
       ),
     );
   }
-  void _passwordAdd(){
-    Navigator.of(context).push(new MaterialPageRoute(builder: (context){
+
+  /// add  new account info
+  void _accountAdd() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
       return new Scaffold(
         appBar: new AppBar(
           title: new Text("Add New Acount"),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.crop_free), onPressed: null),
+          ],
         ),
-        body: new Text("this is test"),
+        body: new Container(
+          padding: const EdgeInsets.all(16.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new TextField(
+                decoration: new InputDecoration(
+                  hintText: "Enter Your Account Name",
+                  labelText: "Account Name:",
+                ),
+              ),
+              new TextField(
+                keyboardType: TextInputType.multiline,
+                decoration: new InputDecoration(
+                  hintText: "Tag",
+                  labelText: "Tag:",
+                ),
+              ),
+              new TextField(
+                obscureText: true,
+                decoration: new InputDecoration(
+                    hintText: "Password",
+                    labelText: "Password:"
+                ),
+              ),
+              new Center(
+                child: new RaisedButton(
+                  onPressed: null,
+                  textTheme: ButtonTextTheme.primary,
+                  child: new Text("Add Account"),
+                ),
+              )
+            ],
+          ),
+        ),
       );
     }));
   }
