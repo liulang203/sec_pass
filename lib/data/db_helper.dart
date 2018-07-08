5dc2c02378857c4c5444ca00a2cea1df1aa2de90
Ding <liulang203@163.com>
2018-07-08 23:44:48 +0800

    add account model and db helper

---
+++ lib/data/db_helper.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sec_pass/models/sec_account.dart';
+/// database helper  for Security Account
+class DbHelper {
+  static final DbHelper _instance = new DbHelper.internal();
+
+  factory DbHelper() => _instance;
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDb();
+    return _db;
+  }
+
+  DbHelper.internal();
+
+  initDb() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, "main.db");
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute("""CREATE TABLE $tableSecAccount(
+      $columnId INTEGER PRIMARY KEY,
+      $columnUsername TEXT,
+      $columnTag TEXT,
+      $columnPassword TEXT,
+      $columnCreatedDate INTEGER,
+      $columnUpdatedDate INTEGER,
+      $columnAlternate1 TEXT,
+      $columnAlternate2 TEXT,
+      $columnAlternate3 TEXT,
+      $columnAlternate4 TEXT,
+      )""");
+  }
+
+  Future<int> saveSecAccount(SecAccount account) async {
+    var dbClient = await db;
+    int res = await dbClient.insert(tableSecAccount, account.toMap());
+    return res;
+  }
+
+  Future<int> delete(int id) async {
+    var dbClient = await db;
+    int res = await dbClient
+        .delete(tableSecAccount, where: "$columnId = ?", whereArgs: [id]);
+    return res;
+  }
+
+  Future<SecAccount> get(int id) async {
+    var dbClient = await db;
+    List<Map> maps = await dbClient
+        .query(tableSecAccount, where: "$columnId = ?", whereArgs: [id]);
+    if (maps.isNotEmpty) {
+      return new SecAccount.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<SecAccount>> search(String condition) async {
+    var dbClient = await db;
+    List<Map> maps = await dbClient.query(tableSecAccount,
+        where: "$columnUsername like ? or $columnTag like ?",
+        whereArgs: [condition, condition]);
+    return maps.map((m) => new SecAccount.fromMap(m)).toList(growable: false);
+  }
+
+  Future<int> updatePassword(SecAccount account) async {
+    var dbClient = await db;
+    int res = await dbClient.update(
+        tableSecAccount,
+        {
+          columnPassword: account.password,
+          columnPassword: account.updatedDate.millisecondsSinceEpoch
+        },
+        where: "$columnId = ?",
+        whereArgs: [account.id]);
+    return res;
+  }
+
+  Future<List<int>> batchUpdatePassword(List<SecAccount> accounts) async {
+    var dbClient = await db;
+    var batch = dbClient.batch();
+    accounts.forEach((account) => batch.update(
+        tableSecAccount,
+        {
+          columnPassword: account.password,
+          columnPassword: account.updatedDate.millisecondsSinceEpoch
+        },
+        where: "$columnId = ?",
+        whereArgs: [account.id]));
+
+    List<int> res = await batch.commit();
+    return res;
+  }
+}
---
+++ lib/models/sec_account.dart
@@ -0,0 +1,77 @@
+final String tableSecAccount = "SecAccount";
+
+final String columnId = "id";
+final String columnUsername = "username";
+final String columnTag = "tag";
+final String columnPassword = "password";
+final String columnCreatedDate = "created_date";
+final String columnUpdatedDate = "updated_date";
+final String columnAlternate1 = "alternate1";
+final String columnAlternate2 = "alternate2";
+final String columnAlternate3 = "alternate3";
+final String columnAlternate4 = "alternate4";
+/// Account model of database
+class SecAccount {
+  /// ID
+  int id;
+
+  /// username
+  String username;
+
+  /// tag for differentiate users
+  String tag;
+
+  /// password
+  String password;
+
+  /// create date
+  DateTime createdDate;
+
+  /// create date
+  DateTime updatedDate;
+
+  /// Alternate field
+  String alternate1;
+
+  /// Alternate field
+  String alternate2;
+
+  /// Alternate field
+  String alternate3;
+
+  /// Alternate field
+  String alternate4;
+
+  Map<String, dynamic> toMap() {
+    Map map = {
+      columnUsername: username,
+      columnTag: tag,
+      columnPassword: password,
+      columnCreatedDate: createdDate.millisecondsSinceEpoch,
+      columnUpdatedDate: updatedDate.millisecondsSinceEpoch,
+      columnAlternate1: alternate1,
+      columnAlternate2: alternate2,
+      columnAlternate3: alternate3,
+      columnAlternate4: alternate4,
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  SecAccount();
+
+  SecAccount.fromMap(dynamic obj) {
+    this.id = obj[columnId];
+    this.username = obj[columnUsername];
+    this.tag = obj[columnTag];
+    this.password = obj[columnPassword];
+    this.createdDate = DateTime.fromMillisecondsSinceEpoch(obj[columnCreatedDate],isUtc: true);
+    this.updatedDate = DateTime.fromMillisecondsSinceEpoch(obj[columnUpdatedDate],isUtc: true);
+    this.alternate1 = obj[columnAlternate1];
+    this.alternate2 = obj[columnAlternate2];
+    this.alternate3 = obj[columnAlternate3];
+    this.alternate4 = obj[columnAlternate4];
+  }
+}
